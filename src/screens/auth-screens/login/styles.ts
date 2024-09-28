33f58ce429156bc867a5fb2d623b5afc156879bc
itsbk1022 <itsbk1022@gmail.com>
2024-09-28 11:13:24 +0500

    all departments

--- src/screens/auth-screens/login/styles.ts
+++ src/screens/auth-screens/login/styles.ts
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: AppColors.black,
+    backgroundColor: '#3333ff',
   },
 });
 export default styles;
